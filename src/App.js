f5b76db9354d9dbca90b1332db6f0d788b4776bd
Joffrey <joyjoffrey@gmail.com>
2022-07-11 21:34:18 +0200

    fix linters erors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { GiHamburgerMenu } from 'react-icons/gi'
+import { GiHamburgerMenu } from 'react-icons/gi';
 import PopUp from './components/PopUp';
 import Todos from './components/Todos';
 import About from './components/About';
 import AboutAuthor from './components/AboutAuthor';
 import AboutApp from './components/AboutApp';
 
-
 function App() {
-  const [menu, setMenu] = useState(true)
+  const [menu, setMenu] = useState(true);
   return (
-    <div className = 'App'>
+    <div className={ 'App' }>
       <BrowserRouter>
-        {menu ? <button onClick = { () => setMenu(false)} className = 'hamburger'><GiHamburgerMenu /></button> : <PopUp setMenu = {setMenu} />}
+        {menu ? <button type={ 'button' } onClick={ () => setMenu(false)} className={ 'hamburger' }><GiHamburgerMenu /></button> : <PopUp setMenu = { setMenu } />}
         <Routes>
-          <Route path = '/' element = {<Todos/>} />
-          <Route path = '/about' element = {<About />} />
-          <Route path = '/about/app' element = {<AboutApp />} />
-          <Route path = '/about/author' element = {<AboutAuthor />} />
+          <Route path={ '/' } element={ <Todos/> } />
+          <Route path={ '/about' } element={ <About /> } />
+          <Route path ={ '/about/app' } element={ <AboutApp /> } />
+          <Route path ={ '/about/author' } element={ <AboutAuthor /> } />
         </Routes>
       </BrowserRouter>
     </div>
